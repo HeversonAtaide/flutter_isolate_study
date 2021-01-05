961847bc65ddc6f7696134e1381bfdb7e930ff39
HeversonAtaide <senaheverson@gmail.com>
2021-01-05 13:45:11 -0300

    comentários adicionados pra ajudar a entender o funcionamento do isolate

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'dart:async';
+import 'dart:isolate';
 
 void main() {
   runApp(MyApp());
@@ -11,19 +13,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
-        // This makes the visual density adapt to the platform that you run
-        // the app on. For desktop platforms, the controls will be smaller and
-        // closer together (more dense) than on mobile platforms.
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
       home: MyHomePage(title: 'Flutter Demo Home Page'),
@@ -33,16 +23,6 @@ class MyApp extends StatelessWidget {
 
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
@@ -50,68 +30,78 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  Isolate _isolate;
+  bool _running = false;
+  static int _counter = 0;
+  String notification = "";
+  ReceivePort _receivePort;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+  void _start() async {
+    _running = true;
+    _receivePort = ReceivePort();
+
+    // _checkTimer é a função assíncrona que vai rodar na thread separada
+    // nesse momento a thread já é aberta e a função começa a rodar em paralelo
+    _isolate = await Isolate.spawn(_checkTimer, _receivePort.sendPort);
+
+    // o _handleMessage é chamado toda vez que a função (_checkTimer) chama o sendPort.send(data);
+    // quando chama o _receivePort.close() aí cai no onDone
+    _receivePort.listen(_handleMessage, onDone: () {
+      print("done!");
     });
+  }
+
+  static void _checkTimer(SendPort sendPort) async {
+    Timer.periodic(new Duration(seconds: 1), (Timer t) {
+      _counter++;
+      String msg = 'notification ' + _counter.toString();
+      print('SEND: ' + msg);
+
+      // esse send manda pra função passada, no caso o _handleMessage(dynamic data)
+      sendPort.send(msg);
+    });
+  }
+
+  void _handleMessage(dynamic data) {
+    print('RECEIVED: ' + data);
+    setState(() {
+      notification = data;
+    });
+  }
+
+  void _stop() {
+    if (_isolate != null) {
+      setState(() {
+        _running = false;
+        notification = '';
+      });
+      _receivePort.close();
+      _isolate.kill(priority: Isolate.immediate);
+      _isolate = null;
+    }
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text(widget.title),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
+      body: new Center(
+        child: new Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+            new Text(
+              notification,
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      floatingActionButton: new FloatingActionButton(
+        onPressed: _running ? _stop : _start,
+        tooltip: _running ? 'Timer stop' : 'Timer start',
+        child: _running ? new Icon(Icons.stop) : new Icon(Icons.play_arrow),
+      ),
     );
   }
 }
